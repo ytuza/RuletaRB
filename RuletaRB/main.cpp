a4ac2e91478edc4dc72a258b3f83e2adbc0aa221
Christian Ytuza <christian.ytuza@ucsp.edu.pe>
2017-02-27 01:06:08 -0500

    hola

--- RuletaRB/main.cpp
+++ RuletaRB/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <random>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int i =0;
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    mt19937 gen(seed);
+    uniform_int_distribution<int> uny(0,35);
+
+    for(i;i<10;i++){
+        cout << uny(gen) << endl;
+    }
     return 0;
 }
